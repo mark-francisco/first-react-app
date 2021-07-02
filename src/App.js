86e61a260fdc39f67b6640743eea6f2709b54f32
Mark Francisco <mfrancisco581@gmail.com>
2021-07-02 00:19:22 -0500

    Add additional components: Modal, Backdrop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore React" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
