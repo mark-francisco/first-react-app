d0e2437d5deea55101e7ae22184d59f060406ece
Mark Francisco <mfrancisco581@gmail.com>
2021-07-13 19:14:56 -0500

    add event props to handle opening and closing the modal

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,5 +1,6 @@
-function Backdrop() {
-  return <div className="backdrop" />;
+function Backdrop(props) {
+  // we are passing a fn value as a prop
+  return <div className="backdrop" onClick={props.onCancel} />;
 }
 
 export default Backdrop;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,19 @@
+// import useState function from node_modules/react
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
-  function deleteHandler() {}
+  // useState() is a React hook. It must be called from inside a React component fn.
+  //useState always returns a 2-element array: [current state, fn that lets you change state]
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
 
   return (
     <div className="card">
@@ -9,6 +23,9 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {/* dynamic expression: in JS, if both conditions are true, the 2nd value will be returned. <Modal /> always returns true. */}
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
